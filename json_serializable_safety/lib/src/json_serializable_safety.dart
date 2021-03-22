48038f6bf72650b325965cc7bcb251c4aa0d8071
zhahao <506902638@qq.com>
2021-03-22 14:22:30 +0800

    增加bool类型判断

--- json_serializable_safety/lib/src/json_serializable_safety.dart
+++ json_serializable_safety/lib/src/json_serializable_safety.dart
@@ -1,3 +1,4 @@
+// 对json解析安全校验
 class JsonSerializableSafety {
   static int jsonToInt(Object v) {
     if (v is int) return v;
@@ -7,9 +8,7 @@ class JsonSerializableSafety {
       int intValue;
       try {
         intValue = int.parse(v);
-      } catch (e) {
-        assert(false, e.toString());
-      }
+      } finally {}
       return intValue;
     } else {
       // ignore: avoid_returning_null
@@ -25,9 +24,7 @@ class JsonSerializableSafety {
       double doubleValue;
       try {
         doubleValue = double.parse(v);
-      } catch (e) {
-        assert(false, e.toString());
-      }
+      } finally {}
       return doubleValue;
     } else {
       // ignore: avoid_returning_null
@@ -35,17 +32,39 @@ class JsonSerializableSafety {
     }
   }
 
-  static String jsonToString(Object v) => v.toString();
-
   // ignore: unnecessary_null_in_if_null_operators
   static num jsonToNum(Object v) => jsonToInt(v) ?? jsonToDouble(v) ?? null;
 
   static bool jsonToBool(Object v) {
     if (v is bool) return v;
-    if (v is int) {
-      return v != 0;
+
+    if (v is String) {
+      final lowerCaseV = v.toLowerCase();
+      if (lowerCaseV == 'true' || lowerCaseV == 'yes') {
+        return true;
+      }
+
+      if (lowerCaseV == 'false' || lowerCaseV == 'no') {
+        return false;
+      }
     }
+
+    final numV = jsonToNum(v)?.toInt();
+    if (numV == 0) {
+      return false;
+    } else if (numV == 1) {
+      return true;
+    }
+
     // ignore: avoid_returning_null
     return null;
+  }
+
+  static String jsonToString(Object v) {
+    if (v is String || v is num || v is bool || v is int || v is double) {
+      return v.toString();
+    } else {
+      return null;
+    }
   }
 }
